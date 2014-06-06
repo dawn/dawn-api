06b386434904509b0f8e3ff172e44adfdb4ae7e5
IceDragon200 <mistdragon100@gmail.com>
2014-06-06 15:01:35 -0500

    Removed CLI from API repo

--- lib/dawn/cli.rb
+++
@@ -1,9 +0,0 @@
-require 'uri'
-require 'net/http'
-require 'time-lord'
-require 'terminal-table'
-
-require 'dawn/api'             # Dawn::Api
-require 'dawn/cli/version'     # CLI::Version information
-require 'dawn/cli/patches'     # Various Monkey Patches
-require 'dawn/cli/application' # Various Monkey Patches
--- lib/dawn/cli/patches.rb
+++
@@ -1,7 +0,0 @@
-class Terminal::Table
-
-  ##
-  # say uses to_str instead of to_s
-  alias :to_str :to_s
-
-end
