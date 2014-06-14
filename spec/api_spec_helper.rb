9398f95a1aeed3288b976ae6d0c2e00ec958c9e2
IceDragon200 <mistdragon100@gmail.com>
2014-06-13 19:49:47 -0500

    Ugly hack to storing stuff in the specs

--- spec/api_spec_helper.rb
+++ spec/api_spec_helper.rb
@@ -1,17 +1,6 @@
 $:.unshift File.expand_path("lib", File.expand_path("../", File.dirname(__FILE__)))
 require "dawn/api"
+require "ostruct"
 require "rspec-expectations"
 
-def validate(obj, key, options)
-  options.each do |validation, value|
-    case validation
-    when :presence
-      assert_true(obj.respond_to?(key))
-      if value
-        assert_not_nil(obj.send(key))
-      else
-        assert_nil(obj.send(key))
-      end
-    end
-  end
-end
+$test_store = OpenStruct.new
