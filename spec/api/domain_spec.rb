b5a20cf4d798f1803950834b0c458653b552e76c
IceDragon200 <mistdragon100@gmail.com>
2014-06-13 19:47:44 -0500

    Updated domain_spec.rb
    Most tests pass using the dawn testing branch.

--- spec/api/domain_spec.rb
+++ spec/api/domain_spec.rb
@@ -1,23 +1,54 @@
 require_relative '../api_spec_helper.rb'
 
 describe Dawn::Domain do
-  let(:domain) { Dawn::Domain }
+  before :all do
+    STDERR.puts "initializing"
+    Dawn::App.safe.create(app: { name: "domain-test-app" })
+    app = $test_store.test_domain_app ||= Dawn::App.find(name: "domain-test-app")
+    $test_store.test_domain1 ||= app.domains.create(domain: { url: "cc-rushers.io" })
+    $test_store.test_domain2 ||= app.domains.create(domain: { url: "cookiecrushers.com" })
+    $test_store.test_domain3 ||= app.domains.create(domain: { url: "cookie-crusher.net" })
+  end
 
-  context "::find" do
-    it "should find 1 domain by id" do
-      domain.find id: 1
+  context ".find" do
+    it "should raise an Excon::Errors::NotFound when Domain cannot be found" do
+      expect { Dawn::Domain.find(id: -1) }.to raise_error(Excon::Errors::NotFound)
     end
+
+    it "should find 1 domain by id" do
+      Dawn::Domain.find id: $test_store.test_domain1.id
+    end
+
     it "should find 1 domain by url" do
-      domain.find url: "cookiecrushers"
+      Dawn::Domain.find url: $test_store.test_domain2.url
+    end
+  end
+
+  context ".destroy" do
+    it "should raise an Excon::Errors::NotFound when Domain cannot be found" do
+      expect { Dawn::Domain.destroy id: -1 }.to raise_error(Excon::Errors::NotFound)
+    end
+
+    it "should destroy 1 domain by id" do
+      Dawn::Domain.destroy id: $test_store.test_domain1.id
+    end
+
+    it "should destroy 1 domain by url" do
+      Dawn::Domain.destroy url: $test_store.test_domain2.url
     end
   end
 
   context "#destroy" do
-    it "should destroy 1 domain by id" do
-      domain.destroy id: 1
+    it "should destroy the domain" do
+      $test_store.test_domain3.destroy
     end
-    it "should destroy 1 domain by url" do
-      domain.destroy url: "cookiecrushers"
-    end
+  end
+
+  after :all do
+    STDERR.puts "cleaning up"
+    $test_store.delete_field(:test_domain1) rescue nil
+    $test_store.delete_field(:test_domain2) rescue nil
+    $test_store.delete_field(:test_domain3) rescue nil
+    $test_store.test_domain_app.destroy rescue nil
   end
 end
