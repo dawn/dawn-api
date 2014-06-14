76152149cd6a2aae33db1225be9db8a9fa130a70
IceDragon200 <mistdragon100@gmail.com>
2014-06-13 19:49:19 -0500

    Update release spec

--- spec/api/release_spec.rb
+++ spec/api/release_spec.rb
@@ -3,21 +3,16 @@ require_relative '../api_spec_helper.rb'
 describe Dawn::Release do
   let(:release) { Dawn::Release }
 
-  context "::find" do
+  context ".find" do
     it "should find 1 release by id" do
       release.find id: 1
     end
   end
 
-  context "#restart" do
-    it "should restart 1 release by id" do
-      release.find id: 1
-    end
-  end
-
-  context "#destroy" do
-    it "should destroy 1 release by id" do
-      release.destroy id: 1
-    end
-  end
+  ## releases cannot be destroyed
+  #context "#destroy" do
+  #  it "should destroy 1 release by id" do
+  #    release.destroy id: 1
+  #  end
+  #end
 end
