c7d3b1c7bc8c1c63f70a8481abd03853380154c2
IceDragon200 <mistdragon100@gmail.com>
2014-07-15 14:15:00 -0500

    Version Bump to 0.11.2

--- lib/dawn/api/version.rb
+++ lib/dawn/api/version.rb
@@ -3,7 +3,7 @@ module Dawn #:nodoc:
     module Version #:nodoc:
       MAJOR = 0
       MINOR = 11
-      PATCH = 1
+      PATCH = 2
       BUILD = nil
       STRING = [[MAJOR, MINOR, PATCH].compact.join("."), BUILD].compact.join("-").freeze
     end
